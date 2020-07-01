d8003c1876f00d3d76ba4e364a3269baf260eae2
Jonathan <jonathanthornton1997@gmail.com>
2020-06-30 17:05:40 -0700

    finished login basic styling

--- src/components/login-signup/signup.jsx
+++ src/components/login-signup/signup.jsx
@@ -1,22 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../actions";
 
-const Signup = props => {
-    return (
-        <form>
-            <input
-                type="text"
-                name="username"
-            />
-            <input
-                type="text"
-                name="email"
-            />
-            <input
-                type="text"
-                name="password"
-            />
-        </form>
-    )
-}
+const initialSignUpValues = {
+  email: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const Signup = (props) => {
+  //Local state for handling form inputs
+  const [signUpValues, setSignUpValues] = useState(initialSignUpValues);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  //Dispatcher for reducer
+  const dispatch = useDispatch();
+
+  //handle functions
+  const handleChanges = (e) => {
+    setSignUpValues({ ...signUpValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const createdUser = {
+      email: signUpValues.email,
+      username: signUpValues.username,
+      password: signUpValues.password,
+    };
+
+    dispatch(registerUser(createdUser));
+    setSignUpValues(initialSignUpValues);
+  };
+
+  //function for checking if password and confirm password are the same
+  useEffect(() => {
+    if (signUpValues.password === signUpValues.confirmPassword) {
+      if (
+        signUpValues.password.length !== 0 ||
+        signUpValues.confirmPassword.length !== 0
+      ) {
+        setButtonDisabled(false);
+      }
+    } else setButtonDisabled(true);
+  }, [signUpValues.password, signUpValues.confirmPassword]);
+
+  return (
+      <div className='signUpFormContainer'>
+    <form onSubmit={handleSubmit}>
+      <div className='innerSignUpFormDiv'>
+      <label>
+        Enter email
+        <input
+          type="text"
+          name="email"
+          value={signUpValues.email}
+          onChange={handleChanges}
+        />
+      </label>
+
+      <label>
+        Create username
+        <input
+          type="text"
+          name="username"
+          value={signUpValues.username}
+          onChange={handleChanges}
+        />
+      </label>
+
+      <label>
+        Enter password
+        <input
+          type="text"
+          name="password"
+          value={signUpValues.password}
+          onChange={handleChanges}
+        />
+      </label>
+
+      <label> Confirm Password
+        
+        <input
+          type="text"
+          name="confirmPassword"
+          value={signUpValues.confirmPassword}
+          onChange={handleChanges}
+        />
+      </label>
+      <div className='loginOrSignUp'>
+      <button disabled={buttonDisabled} onClick={handleSubmit}>
+       Sign up
+      </button>
+      
+      <p>Already have an account?</p>
+      <a href='#'>Login</a>
+      </div>
+      </div>
+    </form>
+    </div>
+  );
+};
 
 export default Signup;
--- src/reducers/generalReducer.js
+++ src/reducers/generalReducer.js
@@ -1,1 +1,13 @@
 // Use this reducer for general functions -- logging in, getting user data, ect.
+
+
+
+const initialGeneralState = {
+    loggedin: false
+}
+export const generalReducer = (state = initialGeneralState, action) => {
+switch(action.type) {
+    
+default:return null
+}
+}
